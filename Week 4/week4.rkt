60e03cbec2752df7d300b1c6165d487d6a8ce016
jaredjstewart <stewart.jared@gmail.com>
2014-07-13 11:55:50 -0500

    in progress

--- Week 4/week4.rkt
+++ Week 4/week4.rkt
@@ -6,24 +6,55 @@
 
 (define (sum xs)
   (apply + xs))
+
+(define filterb
+    (lambda (pred lst)
+      (cond ((null? lst) '())
+            ((pred (car lst)) (cons (car lst) (filterb pred (cdr lst))))
+            (else (filterb pred (cdr lst))))))
+
+(define get-val car)
+(define get-symbol cdr)
+
 ;Conversion
-(define (roman-symbol-to-int symbol)
-  (cond
-    ((char=? symbol #\I) 1)
-    ((char=? symbol #\V) 5)
-    ((char=? symbol #\X) 10)
-    ((char=? symbol #\L) 50)
-    ((char=? symbol #\C) 100)
-    ((char=? symbol #\D) 500)
-    ((char=? symbol #\M) 1000)
-    ))
+(define roman-symbols 
+  (list (cons 1 #\I)
+        (cons 5 #\V)
+        (cons 10 #\X)
+        (cons 50 #\L)
+        (cons 100 #\C)
+        (cons 500 #\D)
+        (cons 1000 #\M)))
 
-(define (roman-to-arabic numeralString)
-    (sum (map roman-symbol-to-int
-                (string->list numeralString))))
+;Roman -> Arabic
+(define (reduce-roman-symbol-to-int symbol symbols)
+  (cond ((null? symbols) 
+         0)
+        ((char=? symbol (get-symbol (car symbols))) 
+         (get-val (car symbols)))
+        (else (reduce-roman-symbol-to-int symbol (cdr symbols)))
+        ))
+
+(define (roman-symbol-to-int symbol) 
+  (reduce-roman-symbol-to-int symbol roman-symbols)) 
+
+(define (roman-numeral-to-arabic numer)
+  (sum (map roman-symbol-to-int (string->list numer))))
+
+;Arabic -> Roman
+
+
+(define (reduce-int-to-roman int numeral)
+  (if (= int 0) numeral)
+  
+  )
 
 
 
-
-(roman-to-arabic "XXV")
+(reduce-int-to-roman 0 "")
+   
+  
+;examples
+(roman-symbol-to-int #\X)
+(roman-numeral-to-arabic "XXV")
 
